988850138ac79ccc2a29c2f5ce9ada5d1fca0032
tangxiangong <tangxiangong@gmail.com>
2025-02-18 09:18:54 +0800

    Reorganize module exports and includes, add Random package

--- src/DiffusionX.jl
+++ src/DiffusionX.jl
@@ -1,8 +1,19 @@
 module DiffusionX
+using Random
+
+export
+    Stable,
+    StochasticProcess,
+    Trajectory,
+    Functional,
+    OccupationTime,
+    TimeAverage,
+    FPT
 
 include("stable.jl")
-include("utils.jl")
-include("simulation/simulation.jl")
-greet() = print("Hello World!")
+include("interfaces.jl")
+# include("utils.jl")
+# include("simulation/simulation.jl")
+# greet() = print("Hello World!")
 
 end # module DiffusionX

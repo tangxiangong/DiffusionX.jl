bb51e8db75178b35b5e7c98f720e8462b0c62680
tangxiangong <tangxiangong@gmail.com>
2025-02-18 11:50:26 +0800

    Add Brownian motion and threading support to module exports
    - Include Bm process in module exports
    - Add Base.Threads to module dependencies
    - Uncomment and include Brownian motion implementation file
    - Expand module exports with simulation and moment-related functions

--- src/DiffusionX.jl
+++ src/DiffusionX.jl
@@ -1,19 +1,28 @@
 module DiffusionX
 using Random
+using Base.Threads
 
 export
+    # interfaces
     Stable,
     StochasticProcess,
     Trajectory,
     Functional,
     OccupationTime,
     TimeAverage,
-    FPT
+    FPT,
+    TAMSD,
+    simulate,
+    moment,
+    𝔼,
+    δ̄²,
+    # processes
+    Bm
 
+
+# files
 include("stable.jl")
 include("interfaces.jl")
-# include("utils.jl")
-# include("simulation/simulation.jl")
-# greet() = print("Hello World!")
+include("bm.jl")
 
 end # module DiffusionX
